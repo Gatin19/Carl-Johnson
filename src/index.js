a4173ae3046094dd686e3007bb8682f0f797e1df
Gatin <154676681+Gatin19@users.noreply.github.com>
2024-09-27 12:29:46 +0000

    atualizei

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const { AoiClient } = require("aoi.js");
+const { AoiVoice, PlayerEvents, PluginName, Cacher, Filter } = require("@aoijs/aoi.music");
+const { Handler } = require("aoi.js-handler");
+require("dotenv").config();
+
+const client = new AoiClient({
+    token: process.env.token,
+    prefix: "c?",
+    intents: ["MessageContent", "Guilds", "GuildMessages", "GuildVoiceStates"],
+    events: ["onMessage", "onInteractionCreate"],
+    database: {
+        type: "aoi.db",
+        db: require("@aoijs/aoi.db"),
+        dbType: "KeyValue",
+        tables: ["main"],
+        securityKey: "a-32-characters-long-string-here",
+    },
+});
+
+const voice = new AoiVoice(client, {
+    requestOptions: {
+        offsetTimeout: 0,
+        soundcloudLikeTrackLimit: 200,
+    },
+    searchOptions: {
+        youtubeClient: "WEB"
+    }
+});
+
+// optional (cacher / filter)
+voice.addPlugin(PluginName.Cacher, new Cacher("memory" /* or "disk" */));
+voice.addPlugin(PluginName.Filter, new Filter({
+    filterFromStart: false
+})),
+
+    voice.bindExecutor(client.functionManager.interpreter);
+
+const handler = new Handler(
+    {
+        client: client,
+        readyLog: true
+    },
+    {
+        // Additional Handler configuration options go here
+    },
+    __dirname
+);
+
+handler.loadCommands('./commands');
+handler.loadVariables('./misc/variables.js')
---
+++ src/misc/variables.js
@@ -0,0 +1,10 @@
+module.exports = {
+    table: 'main', // this is table (optional)
+    variables: {
+        var: 0,
+        var2: "",
+        var3: true,
+        var4: {},
+        var5: 10
+    }
+}
